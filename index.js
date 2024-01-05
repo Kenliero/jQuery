b43700bf49c2fdcc230234f3546add05ccca13b1
Kenliero <118605983+Kenliero@users.noreply.github.com>
2024-01-05 15:29:16 +0200

    using css

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $("h1").css("color", "red");
 
 $("document").keydown(function(event) {
-    //$("h1").text(event.key);
+    //$("h1").text(event.key); // This one works for surem Angela example
     $("h1").css("text", event.key);
 });
