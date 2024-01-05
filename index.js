27a4f07d5e1dbe890603d7e7febc6cf918554783
Kenliero <118605983+Kenliero@users.noreply.github.com>
2024-01-05 15:31:05 +0200

    mouseover version

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 $("h1").css("color", "red");
 
+/*
 $("document").keydown(function(event) {
     //$("h1").text(event.key); // This one works for surem Angela example
-    $("h1").css("text", event.key);
+    $("h1").css("text", event.key); // This is the one I made.
 });
+*/
+
+$("document").on("mouseover", function() {
+    $("h1").css("text", event.key); // This is the one I made.
+})
